571be00a5c5175758fff9ffc2bdf23c8e6a54dcc
Abrar Khan <akhan@nmdp.org>
2025-03-23 16:23:30 -0500

    Add avility to reserver books, add scheduler job to create fines

--- spring-data-dynamodb/src/main/java/com/baeldung/spring/data/dynamodb/controller/LibrariansController.java
+++ spring-data-dynamodb/src/main/java/com/baeldung/spring/data/dynamodb/controller/LibrariansController.java
@@ -1,16 +1,12 @@
 package com.baeldung.spring.data.dynamodb.controller;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.baeldung.spring.data.dynamodb.model.Librarians;
-import com.baeldung.spring.data.dynamodb.model.ProductInfo;
 import com.baeldung.spring.data.dynamodb.repositories.LibrariansRepository;
-import com.baeldung.spring.data.dynamodb.repositories.ProductInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
---
+++ spring-data-dynamodb/src/main/java/com/baeldung/spring/data/dynamodb/request/dto/BookReservation.java
@@ -0,0 +1,32 @@
+package com.baeldung.spring.data.dynamodb.request.dto;
+
+public class BookReservation {
+
+    private String memberId;
+    private String bookId;
+    private String borrowDate;
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBorrowDate() {
+        return borrowDate;
+    }
+
+    public void setBorrowDate(String borrowDate) {
+        this.borrowDate = borrowDate;
+    }
+}
